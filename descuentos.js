4e1adba05cec2026f82d88dc52d3092d4ed4b351
Andres Carrero <andres_car098@hotmail.com>
2021-08-25 22:20:51 -0500

    Implementando HTML

--- descuentos.js
+++ descuentos.js
@@ -8,6 +8,22 @@ function calcularPrecioConDescuento(precio, descuento) {
   return precioConDescuento;
 }
 
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById("InputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = `Precio con descuento: $${precioConDescuento}`;
+}
+
 // console.log({
 //   precioOriginal,
 //   descuento,
